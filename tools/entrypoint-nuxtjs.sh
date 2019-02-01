b58af8cc852b50b53895060ba6706fe81152c69d
Gerardo Junior <me@gerardo-junior.com>
2019-02-01 01:18:07 -0300

    Improving entrypoint

--- tools/entrypoint-nuxtjs.sh
+++ tools/entrypoint-nuxtjs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ -e "$(pwd)/package.json" ]; then
-    if [ ! -d "$(pwd)/.nuxt" ]; then
+if [ -e "${WORKDIR}/package.json" ]; then
+    if [ ! -d "${WORKDIR}/.nuxt" ]; then
         /opt/tools/entrypoint.sh "build"
     fi
 fi
